bbb11980ca1131aef543d540164f46892fb6631b
kawthar safi <kawthar.a.safi@gmail.com>
2025-07-05 01:22:53 +0300

    translate service

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { I18nService } from '../../../service/i18n/i18n.service';
+import { TranslatePipe } from '../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  i18nService = inject(I18nService);
+  language: 'en' | 'ar' = 'en';
+  switchToArabic() {
+    this.language = 'ar';
+    this.i18nService.loadTranslations(this.language);
+  }
+
+  switchToEnglish() {
+    this.language = 'en';
+    this.i18nService.loadTranslations(this.language);
+  }
+}
